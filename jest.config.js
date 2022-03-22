dd0840ae7ac4bbab63e5e0a1b81f110a6e104f94
Krystian Nowak <krystian.k.nowak@gmail.com>
2022-03-22 19:25:39 +0100

    resolve css files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy', // return string for the path that was acces
     '\\.css$': require.resolve('./test/style-mock.js'), // anything that ends with css(regex)
   },
 }
